a90d9d88e13b414b37c28be25d02ec24e9f99235
Sejin Ahmed <mern.sejin@gmail.com>
2026-08-07 05:16:30 +0600

    update: registry component path

--- src/actions/fetch-registry.ts
+++ src/actions/fetch-registry.ts
@@ -1,7 +1,6 @@
 import { registryURL } from '../registry-url.js';
 export default async function getRegistryItem (component: string) {
-    const path = component.replace('@jp-lab', '');
-    const url = `${registryURL}/registry/${path}.json`;  
+    const url = `${registryURL}/registry/${component}.json`;  
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error(
